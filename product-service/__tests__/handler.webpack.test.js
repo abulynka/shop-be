a0e8d0f2ae5edb0592f6f71593fbb05d0e498d94
Aliaksandr Bulynka <abulynka@gmail.com>
2021-08-29 13:40:07 +0300

    feat: add tests

---
+++ product-service/__tests__/handler.webpack.test.js
@@ -0,0 +1,41 @@
+import { expect } from '@jest/globals';
+import { getProductsById, getProductsList } from '../dist/handler';
+
+test('getProductsById', async () => {
+    return getProductsById().then(
+        (result) => {
+            return expect(result).toStrictEqual({
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        productName: 'car1',
+                        price: 222,
+                    }
+                ),
+              });
+        }
+    );
+});
+
+test('getProductsList', () => {
+    return getProductsList().then(
+        (result) => {
+            return expect(result).toStrictEqual({
+                    statusCode: 200,
+                    body:
+                        JSON.stringify (
+                            [
+                                {
+                                    productName: 'car1',
+                                    price: 111
+                                },
+                                {
+                                    productName: 'car2',
+                                    price: 222
+                                }
+                            ]
+                        )
+                  });
+        }
+    );
+});
