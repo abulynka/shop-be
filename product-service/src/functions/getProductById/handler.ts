af6dde39631e5d41df32acc641ac1d01e94fb68c
Aliaksandr Bulynka <abulynka@gmail.com>
2021-09-08 22:52:12 +0300

    feat: change product by id

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,37 +1,19 @@
 import 'source-map-support/register';
 
-import { formatJSONResponse, formatJSONResponseNotFound, ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
+import { formatJSONResponse, ValidatedAPIGatewayProxyEvent, ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
+import { Products } from '@libs/models/Products';
+import Logger from '@libs/log/logger';
 
-const getProductById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const productId = event.pathParameters['productId'];
+const getProductById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event: ValidatedAPIGatewayProxyEvent<string>) => {
+  await new Logger().logEvent(event);
 
-  if (productId == '1') {
-    return formatJSONResponse(
-      {
-        id: 1,
-        title: 'car1 title',
-        description: 'car1 description',
-        price: 111
-      }
-    );
-
-  } else if (productId == '2') {
-    return formatJSONResponse(
-      {
-        id: 2,
-        title: 'car2 title',
-        description: 'car2 description',
-        price: 222
-      }
-    );
-  }
-
-  return formatJSONResponseNotFound({
-    message: 'Product Id = ' + productId + ' not found'
-  });
+  return formatJSONResponse(
+    await new Products()
+      .getProductById(event.pathParameters['productId'])
+  );
 }
 
 export const main = middyfy(getProductById);
